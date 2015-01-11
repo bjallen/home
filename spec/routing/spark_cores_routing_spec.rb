5cc079e081dc3f4dec77cb382107fddc74add3ba
B.J. Allen <ballen@numerex.com>
2015-01-11 16:53:51 -0600

    clean up style

--- spec/routing/spark_cores_routing_spec.rb
+++ spec/routing/spark_cores_routing_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe SparkCoresController, :type => :routing do
   describe "routing" do
-
     it "routes to #index" do
       expect(:get => "/spark_cores").to route_to("spark_cores#index")
     end
@@ -26,6 +25,5 @@ RSpec.describe SparkCoresController, :type => :routing do
     it "routes to #destroy" do
       expect(:delete => "/spark_cores/1").to route_to("spark_cores#destroy", :id => "1")
     end
-
   end
 end
