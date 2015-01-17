3cf3ed45fe892b675b5f51d84c4162d39b85f3e7
B.J. Allen <ballen@numerex.com>
2015-01-17 10:14:20 -0600

    add sub-resource for thermometer readings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :thermometers, :except => [:show]
+  resources :thermometers, :except => [:show] do
+    resources :temperature_readings, :only => [:index]
+  end
   resources :spark_cores, :except => [:show]
   resources :temperature_readings, :only => [:index]
   get 'availability/check' => 'availability#check'
