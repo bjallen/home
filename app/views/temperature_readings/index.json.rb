88338d8e16a1305f21a329a36aaaddfd0ba59358
B.J. Allen <ballen@numerex.com>
2015-01-17 09:44:20 -0600

    fix parse error in readings view

--- app/views/temperature_readings/index.json.rb
+++ app/views/temperature_readings/index.json.rb
@@ -1,3 +1,3 @@
-@avg_temp_by_time.each do |hour, temp| %>
- convert_celsius_to_fahrenheit(temp).to_s + " °F" => Time.strptime(hour, "%Y-%m-%dT%H").strftime("%I:%M %p, %A %b %d %Y")
+@avg_temp_by_time.each do |hour, temp|
+ "convert_celsius_to_fahrenheit(temp).to_s °F" => Time.strptime(hour, "%Y-%m-%dT%H").strftime("%I:%M %p, %A %b %d %Y")
 end.to_json
