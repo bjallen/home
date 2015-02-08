744c9ac1ac25189f947e33e65117aed6c6e7f44f
B.J. Allen <ballen@numerex.com>
2015-02-08 14:20:11 -0600

    separate room vars

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,10 @@
 $(function () {
-    d3.json('/thermometers/1/temperature_readings.json', function(data) {
-        data = MG.convert.date(data, 'time', '%Y-%m-%dT%H:%M:%SZ');
-        data = MG.convert.number(data, 'fahrenheit');
+    d3.json('/thermometers/1/temperature_readings.json', function(data1) {
+        data1 = MG.convert.date(data1, 'time', '%Y-%m-%dT%H:%M:%SZ');
+        data1 = MG.convert.number(data1, 'fahrenheit');
 
         MG.data_graphic({
-          data: data,
+          data: data1,
           width: $('.result').width(),
           height: $('.result').height(),
           target: ".room-1",
@@ -14,12 +14,12 @@ $(function () {
         });
     });
 
-    d3.json('/thermometers/2/temperature_readings.json', function(data) {
-        data = MG.convert.date(data, 'time', '%Y-%m-%dT%H:%M:%SZ');
-        data = MG.convert.number(data, 'fahrenheit');
+    d3.json('/thermometers/2/temperature_readings.json', function(data2) {
+        data2 = MG.convert.date(data2, 'time', '%Y-%m-%dT%H:%M:%SZ');
+        data2 = MG.convert.number(data2, 'fahrenheit');
 
         MG.data_graphic({
-          data: data,
+          data: data2,
           width: $('.result').width(),
           height: $('.result').height(),
           target: ".room-2",
