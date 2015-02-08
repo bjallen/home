0284c23baf87ee04a7e2fe8c4beced02c66aedf3
B.J. Allen <ballen@numerex.com>
2015-02-08 14:16:53 -0600

    show graphs for all rooms

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,7 +7,22 @@ $(function () {
           data: data,
           width: $('.result').width(),
           height: $('.result').height(),
-          target: ".result",
+          target: ".room-1",
+          x_accessor: "time",
+          y_accessor: "fahrenheit",
+          interpolate: "monotone"
+        });
+    });
+
+    d3.json('/thermometers/2/temperature_readings.json', function(data) {
+        data = MG.convert.date(data, 'time', '%Y-%m-%dT%H:%M:%SZ');
+        data = MG.convert.number(data, 'fahrenheit');
+
+        MG.data_graphic({
+          data: data,
+          width: $('.result').width(),
+          height: $('.result').height(),
+          target: ".room-2",
           x_accessor: "time",
           y_accessor: "fahrenheit",
           interpolate: "monotone"
